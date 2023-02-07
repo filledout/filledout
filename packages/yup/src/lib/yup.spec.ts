3136c1f067d3e49b15a495148f6fe517fba9ddde
ein1x3x <ein1x3x.dev@gmail.com>
2023-02-07 22:24:22 +0400

    feat: basic structure

---
+++ packages/yup/src/lib/yup.spec.ts
@@ -0,0 +1,7 @@
+import { yup } from './yup';
+
+describe('yup', () => {
+  it('should work', () => {
+    expect(yup()).toEqual('yup');
+  });
+});
---
+++ packages/yup/src/lib/yup.ts
@@ -0,0 +1,3 @@
+export function yup(): string {
+  return 'yup';
+}
