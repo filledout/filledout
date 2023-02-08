7853f4387eee34a661b666d0d25b03ccd8f1ffb4
ein1x3x <ein1x3x.dev@gmail.com>
2023-02-09 01:38:48 +0400

    feat: main page docs

--- apps/website/.vitepress/config.ts
+++ apps/website/.vitepress/config.ts
@@ -2,33 +2,121 @@ import { defineConfig } from 'vitepress';
 
 export default defineConfig({
   lang: 'en-US',
-  title: 'Nx VitePress',
-  description: 'Vite & Vue powered static site generator.',
+
+  title: 'FilledOut ☄️🥛',
+
+  description: 'UI Framework-agnostic effector based form library',
 
   srcDir: 'docs',
 
   themeConfig: {
     nav: [
-      { text: 'Guide', link: '/guide/', activeMatch: '^/$|^/guide/' },
+      {
+        text: 'Guide',
+
+        link: '/guide/getting-started',
+
+        activeMatch: '^/$|^/guide/'
+      },
+
       {
         text: 'API',
-        link: '/guide/'
+
+        link: '/api/lib',
+
+        activeMatch: '^/$|^/api/'
       }
     ],
 
     sidebar: {
-      '/guide/': getGuideSidebar()
+      '/api': api(),
+
+      '/guide/': guide()
     }
   }
 });
 
-function getGuideSidebar() {
+function api() {
   return [
     {
-      text: 'Introduction',
-      children: [
+      text: 'Core',
+
+      items: [
+        { text: 'Lib', link: '/api/lib' },
+
+        { text: 'Form', link: '/api/form' },
+
+        { text: 'Field', link: '/api/field' }
+      ]
+    },
+
+    {
+      text: 'Bindings',
+
+      items: [
+        {
+          text: 'React',
+
+          link: '/api/react'
+        }
+      ]
+    }
+  ];
+}
+
+function guide() {
+  return [
+    {
+      text: 'Guide',
+
+      items: [
         { text: 'Getting Started', link: '/guide/getting-started' },
-        { text: 'Configuration', link: '/guide/configuration' }
+
+        { text: 'Setup', link: '/guide/setup' },
+
+        { text: 'Fields & Data Structure', link: '/guide/fields' },
+
+        { text: 'Factory Extension', link: '/guide/factory-extension' },
+
+        { text: 'Validation', link: '/guide/validation' },
+
+        { text: 'Examples', link: '/guide/examples' }
+      ]
+    },
+
+    {
+      text: 'Bindings',
+
+      items: [{ text: 'React', link: '/guide/bindings/react' }]
+    },
+
+    {
+      text: 'Recipes',
+
+      items: [
+        {
+          text: 'Field HOC',
+
+          link: '/guide/recipe/hoc'
+        },
+
+        {
+          text: 'Simple form',
+
+          link: '/guide/recipe/hoc'
+        },
+
+        {
+          text: 'Dynamic Validation',
+
+          link: '/guide/recipe/dynamic-validation'
+        },
+
+        {
+          text: 'Localization',
+
+          link: '/guide/recipe/localization'
+        }
       ]
     }
   ];
--- apps/website/docs/index.md
+++ apps/website/docs/index.md
@@ -1,16 +1,60 @@
 ---
-home: true
-heroImage: /logo.png
-heroAlt: Logo image
-heroText: Vitepress
-tagline: Nx powered project
-actionText: Get Started
-actionLink: /guide/
+layout: home
+
+hero:
+  name: Filledout
+  text: Forms with ease
+  tagline: Dynamic forms powered by Effector
+  image:
+    src: https://media.tenor.com/8kIoZ3dNzjcAAAAC/peepo-peepoblush.gif
+    alt: Filledout
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/getting-started
+    - theme: alt
+      text: API
+      link: /api/lib
+
 features:
-  - title: Theming
-    details: Dark and light mode are already activated.
-  - title: Navigation
-    details: Enjoy having a ready-to-use navigation bar.
-  - title: Sidebar
-    details: Custom sidebar defined for _/guide_ route.
+  - icon: ☂️
+    title: UI Framework-agnostic
+    details: Most forms are usually bound to a certain framework/library and do not exist outside of ui framework's context. Filledout is standalone library with a separate ui bindings.
+  - icon: 🔒
+    title: Typesafe
+    details: Tired of using string paths as field keys? Proxy powered fully typed fields refrence solves the problem.
+  - icon: 💨
+    title: Feature packed
+    details: Has most demanded features out of the box and ready to extend if there's missing ones.
 ---
+
+
+<script setup>
+import {
+  VPTeamPage,
+  VPTeamPageTitle,
+  VPTeamMembers,
+  VPTeamPageSection
+} from 'vitepress/theme'
+
+const members = [
+  {
+    avatar: '/hyze2d-photo.jpg',
+    name: 'Anton Skorochkin',
+    links: [
+      { icon: 'github', link: 'https://github.com/hyze2d' },
+      
+      { icon: 'twitter', link: 'https://twitter.com/hyze2dev' }
+    ]
+  }
+]
+</script>
+
+<VPTeamPage>
+  <VPTeamPageTitle>
+    <template #title>
+      Team
+    </template>
+  </VPTeamPageTitle>
+  <VPTeamMembers :members="members" />
+</VPTeamPage>
